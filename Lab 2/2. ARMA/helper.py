ab58d915b82abfab8a1d15e83c9629a6735b9fe5
dallaart <davidallaart@gmail.com>
2019-06-04 16:54:01 +0200

    dump

---
+++ Lab 2/2. ARMA/helper.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_time_slice(df, date_from, date_to):
+    datemask = (df.index >= date_from) & (df.index <= date_to)
+    return df.loc[datemask].copy()
+
+#The below function was borrowed from Yair Beer on StackOverflow. It is basically a modified version of autocorrelation_plot,
+# which allows to also pass the amount of the amount of samples, which is makes plotting a lot easier. My personal addition
+# was to set the default of n_samples to 10 instead of None. For more information, please see:
+# https://stackoverflow.com/questions/38503381/set-number-of-lags-in-python-pandas-autocorrelation-plot, visited 03-06-2019. 
+from pandas.compat import lmap
+
+def autocorrelation_plot(series, n_samples=10,ax=None, **kwds):
+    """Autocorrelation plot for time series.
+
+    Parameters:
+    -----------
+    series: Time series
+    ax: Matplotlib axis object, optional
+    kwds : keywords
+        Options to pass to matplotlib plotting method
+
+    Returns:
+    -----------
+    ax: Matplotlib axis object
+    """
+    import matplotlib.pyplot as plt
+    n = len(series)
+    data = np.asarray(series)
+    if ax is None:
+        ax = plt.gca(xlim=(1, n_samples), ylim=(-1.0, 1.0))
+    mean = np.mean(data)
+    c0 = np.sum((data - mean) ** 2) / float(n)
+
+    def r(h):
+        return ((data[:n - h] - mean) *
+                (data[h:] - mean)).sum() / float(n) / c0
+    x = (np.arange(n) + 1).astype(int)
+    y = lmap(r, x)
+    z95 = 1.959963984540054
+    z99 = 2.5758293035489004
+    ax.axhline(y=z99 / np.sqrt(n), linestyle='--', color='grey')
+    ax.axhline(y=z95 / np.sqrt(n), color='grey')
+    ax.axhline(y=0.0, color='black')
+    ax.axhline(y=-z95 / np.sqrt(n), color='grey')
+    ax.axhline(y=-z99 / np.sqrt(n), linestyle='--', color='grey')
+    ax.set_xlabel("Lag")
+    ax.set_ylabel("Autocorrelation")
+    if n_samples:
+        ax.plot(x[:n_samples], y[:n_samples], **kwds)
+    else:
+        ax.plot(x, y, **kwds)
+    if 'label' in kwds:
+        ax.legend()
+    ax.grid()
+    return ax
