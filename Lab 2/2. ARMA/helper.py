0d51e604a447ea3e50bad8b635b574b60cc65eef
dallaart <davidallaart@gmail.com>
2019-06-04 18:27:34 +0200

    Final stuff. It's a mess

--- Lab 2/2. ARMA/helper.py
+++ Lab 2/2. ARMA/helper.py
@@ -3,6 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+from sklearn import preprocessing
+from sklearn import decomposition
+from sklearn.metrics import confusion_matrix
+
+
 def get_time_slice(df, date_from, date_to):
     datemask = (df.index >= date_from) & (df.index <= date_to)
     return df.loc[datemask].copy()
@@ -12,6 +17,45 @@ def get_time_slice(df, date_from, date_to):
 # was to set the default of n_samples to 10 instead of None. For more information, please see:
 # https://stackoverflow.com/questions/38503381/set-number-of-lags-in-python-pandas-autocorrelation-plot, visited 03-06-2019. 
 from pandas.compat import lmap
+
+
+def load_dataset(path, has_labels=True):
+    df = pd.read_csv(path)
+
+    # Convert datetime indices
+    df['DATETIME'] = pd.to_datetime(df['DATETIME'], format='%d/%m/%y %H')
+    df.set_index('DATETIME', inplace=True)
+
+    labels = None
+    if has_labels:
+
+        # Extract labels
+        labels = df['ATT_FLAG'].copy()
+        df.drop(columns=['ATT_FLAG'], inplace=True)
+
+        # Convert labels to sensable values
+        labels[labels != 1] = 0
+    
+    return df, labels
+
+def get_test_labels(X_index):
+    
+    attacks = [
+        ('2017-01-16 09:00:00', '2017-01-19 06:00:00'),
+        ('2017-01-30 08:00:00', '2017-02-02 00:00:00'),
+        ('2017-02-09 03:00:00', '2017-02-10 09:00:00'),
+        ('2017-02-12 01:00:00', '2017-02-13 07:00:00'),
+        ('2017-02-24 05:00:00', '2017-02-28 08:00:00'),
+        ('2017-03-10 14:00:00', '2017-03-13 21:00:00'),
+        ('2017-03-25 20:00:00', '2017-03-27 01:00:00')
+    ]
+    
+    y = pd.Series(np.zeros(len(X_index)), index=X_index)
+    for date_from, date_to in attacks:
+        y.loc[(y.index >= date_from) & (y.index <= date_to)] = 1
+        
+    return y
+
 
 def autocorrelation_plot(series, n_samples=10,ax=None, **kwds):
     """Autocorrelation plot for time series.
